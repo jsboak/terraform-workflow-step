990078c10e6702208553e7d98be8d319f869a866
Jake Cohen <jcohen1@pagerduty.com>
2025-04-10 14:06:04 -0700

    remove legacy runCommand method

--- src/main/groovy/com/plugin/terraform/TerraformCommandExecutor.groovy
+++ src/main/groovy/com/plugin/terraform/TerraformCommandExecutor.groovy
@@ -173,27 +173,4 @@ class TerraformCommandExecutor {
             }
         }
     }
-
-//    private static ProcessResult runCommand(PluginStepContext context, File workDir,
-//                                            Map<String, String> env, String terraformPath,
-//                                            List<String> args) {
-//        List<String> cmdArgs = [terraformPath] + args
-//
-//        ProcessBuilder process = new ProcessBuilder(cmdArgs)
-//                .directory(workDir)
-//                .redirectErrorStream(true)
-//        process.environment().clear()
-//        process.environment().putAll(env)
-//
-//        Process proc = process.start()
-//        StringBuilder output = new StringBuilder()
-//        proc.inputStream.withReader { reader ->
-//            reader.eachLine { line ->
-//                output.append(line).append("\n")
-//                context.logger.log(2, line)
-//            }
-//        }
-//        proc.waitFor()
-//        return new ProcessResult(proc.exitValue(), output.toString())
-//    }
 }
